fa0fcbd9d56d4d79444ee856abbd2e78486144b4
Cesar-Moran <cesarmorangithub@gmail.com>
2023-11-21 16:45:16 -0500

    Remaked 'a que nos dedicamos' cards

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,26 +1,33 @@
 import { ArrowDown } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export const Homepage = () => {
+  const [firstCardIsClicked, setFirstCardClicked] = useState(false);
+  const [secondCardIsClicked, setSecondCardClicked] = useState(false);
+  const [thirdCardIsClicked, setThirdCardClicked] = useState(false);
+
   return (
     <article className="z-10">
-      <section
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1436473849883-bb3464c23e93?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
-        }}
-      >
+      <section className="hero min-h-screen bg-[url(https://media.gettyimages.com/id/1259269839/es/foto/encendiendo-el-aire-acondicionado.jpg?s=612x612&w=0&k=20&c=utruz5SdpWfZaoQtjepqzB38aH2gyDmkVQN7qNDdDdY=)] bg-cover ">
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <div>
               {" "}
-              <h1 className="mb-5 text-5xl font-bold">Hola, bienvenido!</h1>
-              <p className="mb-5">
+              <h1 className="mb-5 text-5xl font-bold text-yellow-400">
+                Hola, bienvenido!
+              </h1>
+              <p className="mb-5 text-white">
                 Productos de calidad y reparaciones expertas para tu hogar.
                 Confía en nosotros, cuidamos de lo que más importa.
               </p>
+              <Link to={"/outletzone/contacto"}>
+                <button className="btn bg-yellow-400 border-none text-white">
+                  Contactanos
+                </button>
+              </Link>
             </div>
             <div className="flex gap-4 items-center justify-center">
               <ArrowDown className="animate-pulse translate-y-80" size={50} />
@@ -28,79 +35,132 @@ export const Homepage = () => {
           </div>
         </div>
       </section>
-      <main className=" bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] min-h-screen flex flex-col gap-20 text-center py-20 text-black italic ">
-        <h2 className="text-3xl">¿A qué nos dedicamos?</h2>
-        <section className="flex flex-col px-8">
-          <div className="flex flex-col  gap-4">
-            <div className="glass   gap-8 bg-black text-white p-8 text-start ">
-              <div className="max-w-3xl">
-                <blockquote className="text-lg ">
-                  Electrodomésticos de Calidad para tu Hogar
-                </blockquote>
-                <div className="divider"></div>
-                <p className="max-w-5xl">
-                  En nuestro compromiso de hacer tu vida más fácil, ofrecemos un
-                  servicio integral que abarca desde la venta de
-                  electrodomésticos de alta calidad hasta la reparación experta.
-                  Explora nuestra gama premium de neveras, lavadoras y aires
-                  acondicionados diseñados para mejorar tu hogar. Cuando surge
-                  un problema, nuestro equipo de técnicos especializados está
-                  listo para brindar soluciones rápidas y eficientes. Con
-                  nosotros, obtienes calidad, confiabilidad y comodidad en un
-                  solo lugar. Tu satisfacción y la funcionalidad de tu hogar son
-                  nuestra prioridad número uno.
-                </p>
-              </div>
-            </div>
-
-            <div className="divider divider-horizontal divider-neutral"></div>
-            <div className="flex flex-col md:flex-row gap-8">
-              <div className="glass  bg-black text-white p-8 text-start max-w-7xl">
-                <blockquote className="text-lg ">
-                  Mantenimiento Preventivo
-                </blockquote>
-                <div className="divider"></div>
-                <p>
-                  Enfócate en la prevención para mantener tus electrodomésticos
-                  en su mejor estado. Ofrecemos servicios de mantenimiento
-                  preventivo especializados para neveras, lavadoras y aires
-                  acondicionados. Nuestro equipo de técnicos expertos realiza
-                  revisiones periódicas para evitar problemas futuros y
-                  garantizar un rendimiento óptimo. Asegura la longevidad de tus
-                  electrodomésticos con nuestro enfoque proactivo.
-                </p>
-              </div>
-
-              <img
-                src="https://media.gettyimages.com/id/1309901712/es/foto/female-hand-opens-fridge.jpg?s=612x612&w=0&k=20&c=wg7XVznvQlO5hrB-jwM_2LSEzdtCCMziYapPG6AnFyI="
-                alt=""
-                className="rounded-lg object-cover  h-52"
-              />
-            </div>
-            <div className="divider divider-horizontal divider-neutral"></div>
-
-            <div className="flex flex-col md:flex-row gap-8">
-              <div className="glass  bg-black text-white p-8 text-start max-w-3xl">
-                <div>
-                  <blockquote className="text-lg ">
-                    Soluciones Completas para tu Hogar
-                  </blockquote>
-                  <div className="divider"></div>
-                  <p>
-                    Descubre la excelencia en cada rincón de tu hogar con
-                    nuestra gama de electrodomésticos premium. Desde neveras
-                    elegantes hasta lavadoras eficientes y aires acondicionados
-                    de última generación, ofrecemos productos que combinan
-                    rendimiento, durabilidad y estilo. Transforma tu espacio con
-                    lo mejor en tecnología para una vida más cómoda.
+      <main className=" bg-white  flex flex-col gap-20 text-center py-20 text-black italic ">
+        <h2 className="text-3xl font-semibold ">¿A qué nos dedicamos?</h2>
+        <section className="px-8 flex flex-col md:flex-row gap-4 ">
+          {/* First card */}
+          {/* First card */}
+          {/* First card */}
+          {/* First card */}
+          <div className="  bg-yellow-500 text-white text-start shadow-xl rounded-3xl w-full">
+            <div className="max-w-xl">
+              <blockquote className="text-lg font-semibold px-8 pt-8 ">
+                Electrodomésticos de Calidad para tu Hogar
+              </blockquote>
+              <div className="divider"></div>
+              <button
+                className="w-full"
+                onClick={() => setFirstCardClicked(!firstCardIsClicked)}
+              >
+                {firstCardIsClicked ? (
+                  <p className="mx-auto flex items-center justify-center gap-4  ">
+                    <FaEyeSlash size={60} />
+                    <span className="opacity-0">Leer mas</span>
                   </p>
-                </div>
-              </div>
-              <img
-                src="https://media.gettyimages.com/id/70000521/es/foto/refrigerator.jpg?s=612x612&w=0&k=20&c=ZEWcavoDzzt_Re0ytCS-pCEAXLFP7GwIvat1G1nomKQ="
-                alt=""
-                className="h-60 object-cover rounded-lg"
-              />
+                ) : (
+                  <p className="mx-auto flex items-center justify-center   gap-4 ">
+                    <FaEye size={60} />
+                    <span className="opacity-100 font-medium">Leer mas</span>
+                  </p>
+                )}
+              </button>
+
+              <p
+                className={
+                  firstCardIsClicked
+                    ? "translate-x-0 max-h-[400px] peer-focus:opacity-100  duration-200 p-8"
+                    : "opacity-0 max-h-[400px] -translate-x-3/4  duration-200 p-8"
+                }
+              >
+                Venta y reparación de electrodomésticos de calidad. Descubre
+                nuestra gama premium para tu hogar y obtén soluciones
+                confiables. Tu satisfacción es nuestra prioridad.
+              </p>
+            </div>
+          </div>
+
+          <div className="divider divider-horizontal divider-neutral"></div>
+          {/* Second card */}
+          {/* Second card */}
+          {/* Second card */}
+          {/* Second card */}
+          <div className="  bg-yellow-500 text-white text-start shadow-xl  rounded-3xl  w-full">
+            <div className="max-w-xl">
+              <blockquote className="text-lg font-semibold px-8 pt-8 ">
+                Reparación Confiable para tus Electrodomésticos
+              </blockquote>
+              <div className="divider"></div>
+              <button
+                className="w-full"
+                onClick={() => setSecondCardClicked(!secondCardIsClicked)}
+              >
+                {secondCardIsClicked ? (
+                  <p className="mx-auto flex items-center justify-center gap-4  ">
+                    <FaEyeSlash size={60} />
+                    <span className="opacity-0">Leer mas</span>
+                  </p>
+                ) : (
+                  <p className="mx-auto flex items-center justify-center   gap-4 ">
+                    <FaEye size={60} />
+                    <span className="opacity-100 font-medium">Leer mas</span>
+                  </p>
+                )}
+              </button>
+
+              <p
+                className={
+                  secondCardIsClicked
+                    ? "translate-x-0 max-h-[400px] peer-focus:opacity-100  duration-200 p-8"
+                    : "opacity-0 max-h-[400px] -translate-x-3/4  duration-200 p-8"
+                }
+              >
+                No dejes que un electrodoméstico averiado arruine tu día.
+                Nuestro equipo experto está aquí para reparar tus dispositivos
+                rápidamente. Funcionamiento óptimo, sin complicaciones
+              </p>
+            </div>
+          </div>
+          <div className="divider divider-horizontal divider-neutral"></div>
+
+          {/* Third card */}
+          {/* Third card */}
+          {/* Third card */}
+          {/* Third card */}
+          <div className="  bg-yellow-500 text-white text-start shadow-xl  rounded-3xl w-full">
+            <div className="max-w-xl">
+              <blockquote className="text-lg font-semibold px-8 pt-8 ">
+                Prevención para un Funcionamiento Óptimo
+              </blockquote>
+              <div className="divider"></div>
+              <button
+                className="w-full"
+                onClick={() => setThirdCardClicked(!thirdCardIsClicked)}
+              >
+                {thirdCardIsClicked ? (
+                  <p className="mx-auto flex items-center justify-center gap-4  ">
+                    <FaEyeSlash size={60} />
+                    <span className="opacity-0">Leer mas</span>
+                  </p>
+                ) : (
+                  <p className="mx-auto flex items-center justify-center   gap-4 ">
+                    <FaEye size={60} />
+                    <span className="opacity-100 font-medium">Leer mas</span>
+                  </p>
+                )}
+              </button>
+
+              <p
+                className={
+                  thirdCardIsClicked
+                    ? "translate-x-0 max-h-[400px] peer-focus:opacity-100  duration-200 p-8"
+                    : "opacity-0 max-h-[400px] -translate-x-3/4  duration-200 p-8"
+                }
+              >
+                Previene problemas en tus electrodomésticos con nuestro
+                mantenimiento especializado para neveras, lavadoras y aires
+                acondicionados. Nuestro equipo asegura un rendimiento óptimo y
+                longevidad.
+              </p>
             </div>
           </div>
         </section>
@@ -110,8 +170,10 @@ export const Homepage = () => {
         </Link>
         <h2 className="text-3xl">¿Por qué elegirnos?</h2>
         <section className="flex flex-col md:flex-row gap-4 px-8 relative">
-          <div className="glass bg-black text-white p-8 text-start ">
-            <blockquote className="text-lg ">Experiencia Confiable</blockquote>
+          <div className="glass bg-yellow-400 text-white p-8 text-start shadow-xl ">
+            <blockquote className="text-lg font-semibold">
+              Experiencia Confiable
+            </blockquote>
             <div className="divider"></div>
             <p>
               Con años de experiencia en la industria, nuestro equipo cuenta con
@@ -122,8 +184,8 @@ export const Homepage = () => {
           </div>
 
           <div className="divider divider-horizontal divider-neutral"></div>
-          <div className="glass bg-black text-white p-8 text-start relative top-0 md:top-20">
-            <blockquote className="text-lg ">
+          <div className="glass bg-yellow-400 text-white p-8 text-start shadow-xl relative top-0 md:top-20">
+            <blockquote className="text-lg font-semibold">
               Servicio Rápido y Eficiente
             </blockquote>
             <div className="divider"></div>
@@ -136,8 +198,8 @@ export const Homepage = () => {
           </div>
           <div className="divider divider-horizontal divider-neutral"></div>
 
-          <div className="glass bg-black text-white p-8 text-start relative ">
-            <blockquote className="text-lg ">
+          <div className="glass bg-yellow-400 text-white p-8 text-start shadow-xl relative ">
+            <blockquote className="text-lg font-semibold">
               Compromiso con la Satisfacción del Cliente
             </blockquote>
             <div className="divider"></div>
